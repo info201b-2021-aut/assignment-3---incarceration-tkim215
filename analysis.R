5811186ba2aeef6b1e82e079696bc64851ca651f
tkim215 <tkim215@uw.edu>
2021-11-21 12:39:41 -0800

    changes in analysis

--- analysis.R
+++ analysis.R
@@ -1,2 +1,9 @@
+library(tidyverse)
+
 # Loading Data
-incarceration <- read.csv("C:\\Users\\kimta\\INFO201\\assignment-3---incarceration-tkim215\\incarceration_trends.csv")
+incarceration_df <- read.csv("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv")
+
+# Number of columns and rows 
+obs_incarceration <- nrow(incarceration_df)
+num_features_incarceration <- ncol(incarceration_df)
+
